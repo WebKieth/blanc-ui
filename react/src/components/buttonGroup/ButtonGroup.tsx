8b6fb23f039b5da9bb45143fda27976dc07ccf78
Bejoy <casperyourweb@gmail.com>
2025-02-14 20:51:13 +0300

    add react button group tmpl

---
+++ react/src/components/buttonGroup/ButtonGroup.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { ButtonGroupProps } from "./types";
+
+export const ButtonGroup: FC<ButtonGroupProps> = () => {
+  return <></>
+}
---
+++ react/src/components/buttonGroup/types.ts
@@ -0,0 +1,3 @@
+export type ButtonGroupProps = {
+  style?: string
+}
